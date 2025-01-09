fdb9ce7b1cf62553d40fee45bbc9b63cb34dbb73
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-01-09 23:25:32 +0100

    install and setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/HomePage";
+import NoPage from "./pages/NoPage";
 
 function App() {
   return (
     <>
-      <section className="w-full h-screen bg-white text-black flex flex-col gap-12 font-bold text-2xl justify-center items-center font-poppins p-5 md:text-5xl lg:text-7xl">
-        <h1>ELO-OGHENE</h1>
-        <h1>SOFTWARE ENGINEER</h1>
-      </section>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>{" "}
     </>
   );
 }
