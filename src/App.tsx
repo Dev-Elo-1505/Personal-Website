7eef28c879451318df374f00be6413d1b62ea3e5
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-01-09 22:57:59 +0100

    update home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className="w-full h-screen bg-white text-black flex flex-col gap-12 font-bold text-2xl justify-center items-center font-poppins p-5 md:text-5xl lg:text-7xl">
+        <h1>ELO-OGHENE</h1>
+        <h1>SOFTWARE ENGINEER</h1>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
