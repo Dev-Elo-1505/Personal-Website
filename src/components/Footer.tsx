1ac1a8b82037ea655df076b947bf083343991b5c
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-01-11 21:58:33 +0100

    build footer for desktop

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,13 @@ import { IoLogoVercel } from "react-icons/io5";
 
 const Footer = () => {
   return (
-    <div className="flex justify-between items-center flex-col md:flex-row p-5 text-center border-t border-[#665e5d]">
-      <div className="flex items-center gap-2">Built with <FaReact /> React <IoLogoVercel /> Vercel</div>
-      <div className="text-[#665e5d] mt-3 mb-10">Copyright &copy; Elo-oghene 2025 All Rights Reserved</div>
+    <div className="flex justify-between items-center flex-col md:flex-row py-5 px-10 text-center border-t border-[#665e5d] md:h-32">
+      <div className="flex items-center gap-2">
+        Built with <FaReact /> React <IoLogoVercel /> Vercel
+      </div>
+      <div className="text-[#665e5d] mt-3 md:mt-0 md:mb-0 mb-10">
+        Copyright &copy; Elo-oghene 2025 All Rights Reserved
+      </div>
     </div>
   );
 };
