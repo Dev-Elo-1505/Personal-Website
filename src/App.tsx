33d2deb2207a5b102ec8322bda5816744f1362a5
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-01-11 02:37:13 +0100

    create project and about page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/HomePage";
 import NoPage from "./pages/NoPage";
+import AboutPage from "./pages/AboutPage";
+import ProjectPage from "./pages/ProjectPage";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/project" element={<ProjectPage />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>{" "}
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,15 @@
+import { FaFileDownload } from "react-icons/fa";
+
 const Hero = () => {
   return (
     <section className="m-auto text-center">
       <h1 className="text-4xl font-bold tracking-widest">ELO-OGHENE</h1>
       <h3 className="m-3 tracking-widest">Software Engineer</h3>
-      <div className="flex flex-col space-y-4 justify-center items-center">
-        <button className="bg-black cursor-pointer rounded-full text-white p-3 w-3/4">
-          Download Resume
+      <div className="flex space-y-4 justify-center items-center gap-4">
+        <button className="bg-black cursor-pointer rounded-full text-white p-3 flex gap-2 items-center">
+        <FaFileDownload /> Resume
         </button>
-        <button className="bg-black cursor-pointer rounded-full text-white p-3 w-3/4">
+        <button className="bg-white border border-black cursor-pointer rounded-full text-black p-3">
           Contact me
         </button>
       </div>
