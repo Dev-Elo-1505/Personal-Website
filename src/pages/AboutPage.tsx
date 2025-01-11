58eb40dca874d6493af00c8a881e0a7f4bff1e7c
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-01-11 02:39:43 +0100

    add navbar to all pages

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,9 +1,13 @@
-
+import NavBar from "../components/NavBar";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div className="p-5">
+      <div className="flex flex-col h-screen justify-end items-center p-5">
+        <NavBar />
+      </div>
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,9 +1,13 @@
-
+import NavBar from "../components/NavBar";
 
 const ProjectPage = () => {
   return (
-    <div>ProjectPage</div>
-  )
-}
+    <div className="p-5">
+      <div className="flex flex-col h-screen justify-end items-center p-5">
+        <NavBar />
+      </div>
+    </div>
+  );
+};
 
-export default ProjectPage
+export default ProjectPage;
