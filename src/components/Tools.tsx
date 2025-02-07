4f98b2f433f7af9b209a93a4df75b99ca97a293d
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-02-07 20:43:40 +0100

    convert tool images to svgs

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -1,42 +1,41 @@
-import ReactOriginal from "devicons-react/lib/icons/ReactOriginal";
-import NextjsOriginalWordmark from "devicons-react/lib/icons/NextjsOriginalWordmark";
-import TailwindcssPlainWordmark from "devicons-react/lib/icons/TailwindcssPlainWordmark";
-import TypescriptPlain from "devicons-react/lib/icons/TypescriptPlain";
-import HTML5PlainWordmark from "devicons-react/lib/icons/Html5PlainWordmark";
-import JavascriptPlain from "devicons-react/lib/icons/JavascriptPlain";
-import Css3Plain from "devicons-react/lib/icons/Css3PlainWordmark";
+import html from "../assets/html.svg"
+import css from "../assets/css.svg"
+import js from "../assets/js.svg"
+import react from "../assets/react.svg"
+import nextjs from "../assets/nextjs.svg"
+import tailwind from "../assets/tailwind.svg"
 const stacks = [
-  { id: 1, name: "React", src: <ReactOriginal size={50} /> },
+  {
+    id: 1,
+    src: html,
+    name: "HTML"
+  },
   {
     id: 2,
-    name: "Next.js",
-    src: <NextjsOriginalWordmark size={50} />,
+    src: css,
+    name: "CSS"
   },
   {
     id: 3,
-    name: "TailwindCSS",
-    src: <TailwindcssPlainWordmark size={100} />,
+    src: js,
+    name: "JavaScript"
   },
   {
     id: 4,
-    name: "Typescript",
-    src: <TypescriptPlain size={50} />,
+    src: react,
+    name: "ReactJs"
   },
   {
     id: 5,
-    name: "HTML",
-    src: <HTML5PlainWordmark size={50} />,
+    src: nextjs,
+    name: "NextJs"
   },
   {
     id: 6,
-    name: "JS",
-    src: <JavascriptPlain size={50} />,
+    src: tailwind,
+    name: "TailwindCss"
   },
-  {
-    id: 7,
-    name: "CSS",
-    src: <Css3Plain size={50} />,
-  },
+  
 ];
 
 const Tools = () => {
@@ -45,7 +44,7 @@ const Tools = () => {
       <div className="flex animate-infinite-scroll gap-10 bg-[#f5f5f5] py-4 text-black">
         {[...stacks, ...stacks].map((stack) => (
           <div key={stack.id} className="flex items-center gap-10 text-4xl">
-            {stack.src}
+            <img src={stack.src} alt={stack.name} />
           </div>
         ))}
       </div>
