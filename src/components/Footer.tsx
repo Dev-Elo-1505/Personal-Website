bf28f621f3495aa6f017829b6591d55b0b7484cf
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-02-08 22:50:03 +0100

    add icons to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
-import { FaReact } from "react-icons/fa";
-import { IoLogoVercel } from "react-icons/io5";
+import react from "../assets/react.svg";
+import tailwind from "../assets/tailwind.svg";
 
 const Footer = () => {
   return (
     <div className="flex justify-between items-center flex-col md:flex-row py-5 px-10 text-center border-t border-[#665e5d] md:h-32">
       <div className="flex items-center gap-2">
-        Built with <FaReact /> React <IoLogoVercel /> Vercel
+        Built with <img src={react} width="20px" /> React <img src={tailwind} width="20px" /> Tailwind
       </div>
       <div className="text-[#665e5d] mt-3 md:mt-0 md:mb-0 mb-10 text-sm">
         Copyright &copy; Elo-oghene 2025 All Rights Reserved
