0e9f2d211581caa2803c76c8a341db8a63a4057e
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-02-08 01:16:12 +0100

    add more tools

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -1,13 +1,14 @@
 import { stacks } from "../data";
 import { motion } from "motion/react";
+import "../index.css"
 
 const Tools = () => {
   return (
-    <div className="bg-black/5 backdrop-blur">
+    <div className="bg-black/5 backdrop-blur flex MyGradient">
       <motion.div
         initial={{ x: 0 }}
         animate={{ x: "-100%" }}
-        transition={{ duration: 60, repeat: Infinity, ease: "linear" }}
+        transition={{ duration: 30, repeat: Infinity, ease: "linear" }}
         className="flex gap-20 items-center justify-center p-5"
       >
         {stacks.map((stack) => (
--- src/data.ts
+++ src/data.ts
@@ -2,13 +2,17 @@ import linkedin from "./assets/linkedin.png";
 import twitter from "./assets/twitter.png";
 import github from "./assets/GitHub_Invertocat_Dark.svg";
 import gmail from "./assets/gmail.png";
-import html from "./assets/html.svg"
-import css from "./assets/css.svg"
-import js from "./assets/js.svg"
-import react from "./assets/react.svg"
-import nextjs from "./assets/nextjs.svg"
-import tailwind from "./assets/tailwind.svg"
+import html from "./assets/html.svg";
+import css from "./assets/css.svg";
+import js from "./assets/js.svg";
+import react from "./assets/react.svg";
+import nextjs from "./assets/nextjs.svg";
+import tailwind from "./assets/tailwind.svg";
+import firebase from "./assets/firebase.svg"
+import prisma from "./assets/prisma.svg"
+import mysql from "./assets/mysql.svg"
 import todo from "./assets/todo.png";
+
 
 export const navData = [
   { id: 1, name: "Home", href: "/" },
@@ -48,34 +52,53 @@ export const stacks = [
   {
     id: 1,
     src: html,
-    name: "HTML"
+    name: "HTML",
   },
   {
     id: 2,
     src: css,
-    name: "CSS"
+    name: "CSS",
   },
   {
     id: 3,
     src: js,
-    name: "JavaScript"
+    name: "JavaScript",
   },
   {
     id: 4,
     src: react,
-    name: "ReactJs"
+    name: "ReactJs",
   },
   {
     id: 5,
     src: nextjs,
-    name: "NextJs"
+    name: "NextJs",
   },
   {
     id: 6,
     src: tailwind,
-    name: "TailwindCss"
+    name: "TailwindCss",
   },
-  
+  {
+    id: 7,
+    src: github,
+    name: "Github",
+  },
+  {
+    id: 8,
+    src: prisma,
+    name: "Prisma",
+  },
+  {
+    id: 9,
+    src: firebase,
+    name: "Firebase",
+  },
+  {
+    id: 10,
+    src: mysql,
+    name: "MySql",
+  },
 ];
 
 export const projects = [
