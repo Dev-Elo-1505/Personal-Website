6423878e70d75f7580332ad648644492d69f6f39
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-02-08 01:25:31 +0100

    fix: create seamless infinite scrolling for tech stack carousel

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -3,19 +3,22 @@ import { motion } from "motion/react";
 import "../index.css"
 
 const Tools = () => {
+const duplicatedStacks = [...stacks, ...stacks]
+
   return (
-    <div className="bg-black/5 backdrop-blur flex MyGradient">
+    <div className="overflow-hidden bg-black/5 backdrop-blur flex MyGradient">
       <motion.div
         initial={{ x: 0 }}
-        animate={{ x: "-100%" }}
-        transition={{ duration: 30, repeat: Infinity, ease: "linear" }}
+        animate={{ x: "-50%" }}
+        transition={{ duration: 60, repeat: Infinity, ease: "linear" }}
         className="flex gap-20 items-center justify-center p-5"
       >
-        {stacks.map((stack) => (
+        {duplicatedStacks.map((stack) => (
           <img
+          key={stack.id}
             src={stack.src}
             alt={stack.name}
-            width="50px"
+            width="40px"
             title={stack.name}
           />
         ))}
