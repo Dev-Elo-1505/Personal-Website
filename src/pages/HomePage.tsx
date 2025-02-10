044e1231d663b186de4610d82d90d93042a03464
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-02-10 13:03:09 +0100

    clear home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,9 @@
-import About from "../components/About";
-import Hero from "../components/Hero";
-import NavBar from "../components/NavBar";
-import Footer from "../components/Footer";
-import Tools from "../components/Tools";
-import Projects from "../components/Projects";
-import Contact from "../components/Contact";
+
 
 const HomePage = () => {
   return (
-    
-  );
-};
+    <div>HomePage</div>
+  )
+}
 
-export default HomePage;
+export default HomePage
