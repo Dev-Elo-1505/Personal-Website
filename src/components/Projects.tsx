20d03bc7275c81c41e1a2a1acad7c5cfe4447f57
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-02-13 21:01:51 +0100

    style project card

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,15 +8,15 @@ const Projects = () => {
       </div>
       <main className="mt-5 grid grid-cols-1 lg:grid-cols-3 gap-5 place-items-center">
         {projects.map((project) => (
-          <div className="w-72 bg-black/5 backdrop-blur rounded-xl p-5 flex flex-col justify-center">
-            <div className="rounded-xl overflow-hidden">
+          <div className="w-72 bg-black/5 backdrop-blur rounded-xl flex flex-col justify-center">
+            <div className="overflow-hidden rounded-t-xl">
               <img
                 src={project.src}
                 alt=""
-                className="w-full object-contain rounded-sm"
+                className="w-full object-contain"
               />
             </div>
-            <div className="mt-3">
+            <div className="mt-3 p-3">
               <h2 className="font-semibold">{project.name}</h2>
               <p className="mt-3 mb-3">{project.description}</p>
               <div className="flex justify-between items-center">
